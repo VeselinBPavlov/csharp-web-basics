f547cf70d2d8822ca5ae5312bba75054a21c6631
Veselin Pavlov <veselin.b.pavlov@gmail.com>
2019-05-22 07:59:12 +0300

    Added SoftUniHttpServer project for all exercises.

---
+++ 02. SoftUni Information Services - Exercises/src/SIS.HTTP/Headers/HttpHeader.cs
@@ -0,0 +1,23 @@
+﻿using SIS.HTTP.Common;
+
+namespace SIS.HTTP.Headers
+{
+    public class HttpHeader
+    {
+        public const string Cookie = "Cookie";
+
+        public HttpHeader(string key, string value)
+        {
+            CoreValidator.ThrowIfNullOrEmpty(key, nameof(key));
+            CoreValidator.ThrowIfNullOrEmpty(value, nameof(value));
+            this.Key = key;
+            this.Value = value;
+        }
+
+        public string Key { get; }
+
+        public string Value { get; }
+
+        public override string ToString() => $"{this.Key}: {this.Value}";
+    }
+}
